d469532fafae63f1ec3435ec05ff621aba8a2510
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-30 21:01:06 +0300

    TaskTableViewController and TaskListView controller prepared

---
+++ ToDoListApp/Controllers/TaskListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TaskListViewController.swift
+//  ToDoListApp
+//
+//  Created by Евгений Березенцев on 30.07.2021.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    //MARK: - Properties
+
+    
+    
+    //MARK: - Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    
+
+}
--- ToDoListApp/Controllers/TaskTableViewController.swift
+++ ToDoListApp/Controllers/TaskTableViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class TaskTableViewController: UITableViewController {
 
+    //MARK: - Properties
+    
+    
+    
+    
+    //MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,12 +25,12 @@ class TaskTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        0
     }
 
 
