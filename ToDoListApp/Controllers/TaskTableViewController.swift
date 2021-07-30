9242963cd83bfb21a65b65ae5db00ce8b4649787
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-30 20:44:16 +0300

    Added new Controllers

---
+++ ToDoListApp/Controllers/TaskTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskTableViewController.swift
+//  ToDoListApp
+//
+//  Created by Евгений Березенцев on 30.07.2021.
+//
+
+import UIKit
+
+class TaskTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+
+}
